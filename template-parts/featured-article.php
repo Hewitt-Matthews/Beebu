f760177bad1bee3038f02cbf00071acd2a2a477c
Dani-HM <dani@hewittmatthews.co.uk>
2024-03-15 13:10:15 +0000

    Featured Article

---
+++ template-parts/featured-article.php
@@ -0,0 +1,49 @@
+<?php
+
+$featured_article = get_field('featured_article');
+
+if ( $featured_article ) : 
+
+$title = get_the_title($featured_article);
+$date = get_the_date('d/m/Y', $featured_article);
+$featured_image = get_the_post_thumbnail_url($featured_article);
+$copy = wp_trim_words( get_the_excerpt($featured_article), 20, '...' );
+$link = get_the_permalink($featured_article); ?>
+
+  <div class="featured-article section section--spaced">
+
+    <div class="wrapper">
+
+      <h2>Featured post</h2>
+
+      <div class="featured-article__image">
+
+        <?php if( !empty( $featured_image ) ) : ?>
+          <img src="<?= $featured_image ?>" alt="<?= $title ?>" />
+        <?php endif; ?>
+
+      </div>
+
+      <div class="featured-article__inner section--green">
+
+        <?php if ( $title ) : ?>
+          <h2><?= $title ?></h2>
+        <?php endif; ?>
+
+        <?php if ( $date ) : ?>
+          <strong><?= $date ?></strong>
+        <?php endif; ?>
+
+        <?php if ( $copy ) : ?>
+          <?= $copy ?>
+        <?php endif; ?>
+
+        <a class="button" href="<?= $link ?>">Read more</a>
+
+      </div>
+
+    </div>
+
+  </div>
+
+<?php endif; ?>
