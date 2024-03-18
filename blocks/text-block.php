b741ed1b247cfe953f13a74f689c0e4064ed52a3
Dani-HM <dani@hewittmatthews.co.uk>
2024-03-18 11:29:39 +0000

    Text Block

---
+++ blocks/text-block.php
@@ -0,0 +1,30 @@
+<?php
+
+$content = get_sub_field('content');
+$section_options_background_colour = get_sub_field('section_options_background_colour');
+$section_options_curved_section = get_sub_field('section_options_curved_section');
+$section_padding = get_sub_field('section_padding'); ?>
+
+<div class="text-block section
+  <?php echo 'section--' . $section_options_background_colour; ?>
+  <?php if ( $section_padding ) : echo 'section--spaced'; endif; ?>
+  <?php if ( $section_options_curved_section ) : echo 'section--curved'; endif; ?>
+  ">
+
+  <div class="wrapper">
+
+    <div class="text-block">
+
+      <div class="text-block__inner">
+
+        <?php if ( $content ) : ?>
+          <?= $content ?>
+        <?php endif; ?>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</div>
