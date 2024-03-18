73142b108c945efef1ed0bf89073fbce82e09231
Dani-HM <dani@hewittmatthews.co.uk>
2024-03-18 10:59:58 +0000

    404

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php include('header.php'); ?>
+
+<main id="maincontent">
+
+  <div class="wrapper">
+
+    <?php get_template_part('blocks/hero'); ?>
+
+  </div>
+
+</main>
+
+<?php include('footer.php'); ?>
--- blocks/hero.php
+++ blocks/hero.php
@@ -1,26 +1,66 @@
 <?php $rating = 5; 
-$hero_type = get_sub_field('hero_type'); ?>
 
-<div class="hero <?php if ( $hero_type === 'flourish' ) : echo 'flourish-hero'; elseif ( $hero_type === 'green-background' ) : echo 'section--green'; endif; ?>" style="<?php if ( $hero_type === 'background-image' || $hero_type === 'flourish' ) : echo 'background-image: url(' . get_sub_field('background_image')['url'] .')'; endif; ?>">
-    <div class="wrapper">
-        <div class="hero__inner">
-            <h1 class="hero__title"><?=get_sub_field('title'); ?></h1>
-            <p class="hero__copy"><?=get_sub_field('copy'); ?></p>
-            
-            <form class="postcode-search">
-                <input class="postcode-search__input" type="text" placeholder="Enter Postcode" />
-                <button class="postcode-search__button" type="submit">Check your area</button>
-            </form>
+/******************************************************************************************
+ * 404 Page Check
+ ******************************************************************************************/
+if ( is_404() ) :
 
-            <div class="hero__trustpilot">
-                <span>Excellent</span>
-                <div class="reviews__score">
-                    <?php for ($i=0; $i < $rating; $i++): ?>
-                        <span class="reviews__star"><img src="<?=get_template_directory_uri() ?>/assets/img/star.svg" /></span>
-                    <?php endfor; ?>
-                </div>
-                <img src="<?=get_template_directory_uri() ?>/assets/img/trustpilot-white.svg" />
-            </div>
+  $hero_type = get_field('hero_hero_type', 'options');
+  $title = get_field('hero_title', 'options');
+  $copy = get_field('hero_copy', 'options');
+  $postcode_search = get_field('hero_add_postcode_search', 'options');
+  $trustpilot = get_field('hero_add_trustpilot', 'options');
+  $background_image = get_field('hero_background_image', 'options');
+  $add_page_button = get_field('hero_add_page_button', 'options');
+  $button = $add_page_button ? get_field('hero_button', 'options') : '';
+
+else : 
+
+  $hero_type = get_sub_field('hero_type');
+  $title = get_sub_field('title');
+  $copy = get_sub_field('copy');
+  $postcode_search = get_sub_field('add_postcode_search');
+  $trustpilot = get_sub_field('add_trustpilot');
+  $background_image = get_sub_field('background_image');
+  $add_page_button = get_sub_field('add_page_button');
+  $button = $add_page_button ? get_sub_field('button') : '';
+
+endif; ?>
+
+<div class="hero <?php if ( $hero_type === 'flourish' ) : echo 'flourish-hero'; elseif ( $hero_type === 'green-background' ) : echo 'section--green'; endif; ?>" style="<?php if ( $hero_type === 'background-image' || $hero_type === 'flourish' ) : echo 'background-image: url(' . $background_image['url'] .')'; endif; ?>">
+
+  <div class="wrapper">
+
+    <div class="hero__inner">
+
+      <h1 class="hero__title"><?= $title ?></h1>
+      <p class="hero__copy"><?= $copy ?></p>
+      
+      <?php if ( $postcode_search ) : ?>
+        <form class="postcode-search">
+          <input class="postcode-search__input" type="text" placeholder="Enter Postcode" />
+          <button class="postcode-search__button" type="submit">Check your area</button>
+        </form>
+      <?php endif; ?>
+
+      <?php if ( $trustpilot ) : ?>
+        <div class="hero__trustpilot">
+          <span>Excellent</span>
+          <div class="reviews__score">
+            <?php for ($i=0; $i < $rating; $i++): ?>
+                <span class="reviews__star"><img src="<?=get_template_directory_uri() ?>/assets/img/star.svg" /></span>
+            <?php endfor; ?>
+          </div>
+          <img src="<?=get_template_directory_uri() ?>/assets/img/trustpilot-white.svg" />
         </div>
+      <?php endif; ?>
+
+      <?php if ( $add_page_button ) : ?>
+        <a href="<?=$button['url'] ?>" class="button"><?=$button['title'] ?></a>
+      <?php endif; ?>
+        
     </div>
+
+  </div>
+
 </div>
