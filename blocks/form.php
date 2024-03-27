ebf5da20eaeac2571e8a1b55236ed261fcf8d08f
Dani-HM <dani@hewittmatthews.co.uk>
2024-03-27 08:01:06 +0000

    Form CSS

--- blocks/form.php
+++ blocks/form.php
@@ -5,9 +5,16 @@ $form = get_sub_field('form');
 $add_contact_block = get_sub_field('add_contact_block');
 $email = get_field('email_address', 'option');
 $telephone = get_field('telephone', 'option');
-$contact_address = get_field('contact_address', 'option'); ?>
+$contact_address = get_field('contact_address', 'option'); 
+$section_options_background_colour = get_sub_field('section_options_background_colour');
+$section_options_curved_section = get_sub_field('section_options_curved_section');
+$section_padding = get_sub_field('section_padding'); ?>
 
-<div class="form section">
+<div class="form section
+  <?php echo 'section--' . $section_options_background_colour; ?>
+  <?php if ( $section_padding ) : echo 'section--spaced'; endif; ?>
+  <?php if ( $section_options_curved_section ) : echo 'section--curved'; endif; ?>
+  ">
   <div class="wrapper">
     <div class="form__form">
       <?php echo do_shortcode('[gravityform id="' . $form . '" title="false" ajax="true"]'); ?>
@@ -38,15 +45,19 @@ $contact_address = get_field('contact_address', 'option'); ?>
               <p><?= $contact_address['address_postcode'] ?></p>
             <?php endif; ?>
           <?php endif; ?>
-
+          
           <?php if ( $telephone ) : ?>
-            <strong>Telephone</strong>
-            <a class="" href="tel:<?= $telephone ?>"><?= $telephone ?></a>
+            <div class="telephone">
+              <strong>Telephone</strong>
+              <a class="" href="tel:<?= $telephone ?>"><?= $telephone ?></a>
+            </div>
           <?php endif; ?>
 
           <?php if ( $email ) : ?>
-            <strong>Email</strong>
-            <a class="" href="mailto:<?= $email ?>"><?= $email ?></a>
+            <div class="email">
+              <strong>Email</strong>
+              <a class="" href="mailto:<?= $email ?>"><?= $email ?></a>
+            </div>
           <?php endif; ?>
 
         </div>
