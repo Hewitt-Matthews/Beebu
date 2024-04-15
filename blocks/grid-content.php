761f8d47c1bc7ce8bd87f937ab6198d3ee11f227
Dani-HM <dani@hewittmatthews.co.uk>
2024-04-15 13:06:34 +0100

    Grid Content / Form

--- blocks/grid-content.php
+++ blocks/grid-content.php
@@ -1,6 +1,7 @@
 <?php 
 
 $title = get_sub_field('title');
+$grid_width = get_sub_field('grid_width');
 $grid_blocks = get_sub_field('grid_blocks');
 $description = get_sub_field('description');
 $title_description_position = get_sub_field('title_-_description_position');
@@ -18,11 +19,13 @@ $section_padding = get_sub_field('section_padding');
   <?php if ( $section_options_curved_section ) : echo 'section--curved'; endif; ?>
   ">
     <div class="wrapper">
-      <div class="grid-content__inner <?= $alignment ?>">
-        <h2 class="grid-content__title"><?=$title ?></h2>
-        <p class="grid-content__description"><?=$description ?></p>
-      </div>
-      <div class="grid-content__grid">
+      <?php if ( $title || $description ) : ?>
+        <div class="grid-content__inner <?= $alignment ?>">
+          <h2 class="grid-content__title"><?=$title ?></h2>
+          <p class="grid-content__description"><?=$description ?></p>
+        </div>
+      <?php endif; ?>
+      <div class="grid-content__grid grid-content__columns-<?= $grid_width ?>">
         <?php foreach ( $grid_blocks as $block ) : ?>
           <?php if ( $block['page_link'] && $block['link_type'] ) : ?>
             <a href="<?= $block['page_link']['url'] ?>">
