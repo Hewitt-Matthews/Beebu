d01b17991667962923251aaf1f07c4924cbefff3
Dani-HM <dani@hewittmatthews.co.uk>
2024-04-15 15:36:27 +0100

    Content Slider

--- blocks/content-slider.php
+++ blocks/content-slider.php
@@ -11,11 +11,11 @@ $section_padding = get_sub_field('section_padding'); ?>
   <?php echo 'section--' . $section_options_background_colour; ?>
   <?php if ( $section_padding ) : echo 'section--spaced'; endif; ?>
   <?php if ( $section_options_curved_section ) : echo 'section--curved'; endif; ?>
-  ">
+  " data-glide-el="track">
 
   <div class="wrapper">
 
-    <div class="slider__inner">
+    <div class="content-slider__inner">
 
       <div class="meta">
         <h2 class="slider__title"><?=$title ?></h2>
@@ -26,7 +26,7 @@ $section_padding = get_sub_field('section_padding'); ?>
 
     </div>
     
-    <div class="slider__slider">
+    <div class="content-slider__slider slider-wrapper slick">
 
       <?php foreach ( $slides as $slide ) :
             
@@ -36,11 +36,13 @@ $section_padding = get_sub_field('section_padding'); ?>
       
       ?>
 
-        <div class="slider__slide">
-          <img class="slider__image" src="<?= $slide_image['url'] ?>" />
-          <div>
-            <h2 class="slider__title"><?= $title ?></h2>
-            <p class="slider__copy"><?= $copy ?></p>
+        <div class="content-slider__slide slick__slide">
+          <div class="content-slider__content">
+            <img class="slider__image" src="<?= $slide_image['url'] ?>" />
+            <div class="content-slider__meta">
+              <h2 class="content-slider__title"><?= $title ?></h2>
+              <p class="content-slider__copy"><?= $copy ?></p>
+            </div>
           </div>
         </div>
 
@@ -51,3 +53,20 @@ $section_padding = get_sub_field('section_padding'); ?>
   </div>
 
 </div>
+
+<script>
+$( document ).ready(function() {
+	$('.slick').slick({
+	  centerMode: true,
+    slidesToShow: 1,
+	  responsive: [
+      {
+        breakpoint: 980,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+	  ]
+	});
+});
+</script>
