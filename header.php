a39f587acfeea39a3f1edcbf1563f0514517a871
samcoombs <samcoombs11@gmail.com>
2024-06-21 07:55:08 +0100

    404 Logo Change

--- header.php
+++ header.php
@@ -15,23 +15,26 @@
     <div class="wrapper">
         <div class="header__inner">
             <div class="header__logo">
-                <?php 
-                  $nav_img = get_field('header_nav_image');
-                  if ( $nav_img ) { ?>
-                    <a href="/">
+            <?php 
+              $nav_img = get_field('header_nav_image');
+              if ( $nav_img ) { ?>
+                  <a href="/">
                       <img src="<?= $nav_img ?>" />
-                    </a>
-                  <?php } else { ?>
-                    <a href="/">
-                      <?php // 404, home, about, contact
-                      if ( is_404() || is_page('about') || is_page('contact') || is_home() || is_front_page() ) : ?>
-                        <img src="<?=get_template_directory_uri() ?>/assets/img/new-beebu-logo-white.png" />
+                  </a>
+              <?php } else { ?>
+                  <a href="/">
+                      <?php //home, about, contact
+                      if ( is_page('about') || is_page('contact') || is_home() || is_front_page() ) : ?>
+                          <img src="<?=get_template_directory_uri() ?>/assets/img/new-beebu-logo-white.png" />
+                      <?php elseif ( is_404() ) : ?>
+                          <img src="<?=get_template_directory_uri() ?>/assets/img/beebu-logo-white.svg" />
                       <?php else : ?>
-                        <img src="<?=get_template_directory_uri() ?>/assets/img/new-beebu-logo-black.png" />
+                          <img src="<?=get_template_directory_uri() ?>/assets/img/new-beebu-logo-black.png" />
                       <?php endif; ?>
-                    </a>
-                  <?php }
-                ?>
+                  </a>
+              <?php }
+              ?>
+
             </div>
             <nav aria-label="Main Menu">
                 <button class="open-button js-open-nav" aria-expanded="false" aria-haspopup="menu" aria-controls="main-menu" aria-label="Open Main Navigation">
