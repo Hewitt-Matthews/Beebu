ac22c43612f41aef0f2cb7e0760affa22b417a29
Alex McIver <alexmciver@ymail.com>
2024-11-01 18:05:21 +0000

    OK now the slider just needs a few styling tweaks

--- blocks/hero.php
+++ blocks/hero.php
@@ -42,335 +42,93 @@ else :
 endif; ?>
 
 
+<!--Desktop Hero-->
 <div class="hero <?php if ( $hero_type === 'flourish' ) : echo 'flourish-hero'; elseif ( $hero_type === 'green-background' ) : echo 'section--green'; endif; ?>">
-
-  <div class="desktop-slider">
-    <?php 
-    if ($background_images) :
-        foreach ($background_images as $image) : ?>
-            <div class="slide">
-                <div class="hero-content desktop-hero-content" style="background-image: url('<?php echo esc_url($image['background_image']['url']); ?>');">
-                    <div class="wrapper">
-                        <div class="hero__inner">
-                            <h1 class="hero__title"><?= $title ?></h1>
-                            <p class="hero__copy"><?= $copy ?></p>
-                            <?php if ($postcode_search): ?>
-                                <form class="postcode-search" method="GET" action="<?php echo esc_url($availability_check_url); ?>"> 
-                                    <input class="postcode-search__input" type="text" name="postcode" value="<?php echo isset($_GET['postcode']) ? esc_attr($_GET['postcode']) : ''; ?>" placeholder="Enter your postcode" required> 
-                                    <button class="postcode-search__button" type="submit">Check Availability</button>
-                                </form>
-                            <?php endif; ?>
-                            <?php if ( $additional_text_below_the_postcode_search ): ?>
-                                <p class="hero__text-below-postcode"><?= $additional_text_below_the_postcode_search ?></p>
-                            <?php endif; ?>
-                            <?php if ( $add_page_button ) : ?>
-                                <a href="<?=$button['url'] ?>" class="button <?php if ( $hero_type === 'flourish' ) : ?> button--black <?php endif; ?>"><?=$button['title'] ?></a>
-                            <?php endif; ?>
-                            <?php if ( $trustpilot ) : ?>
-                                <div class="hero__trustpilot">
-                                    <!-- TrustBox script -->
-                                    <script type="text/javascript" src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" async></script>
-                                    <!-- End TrustBox script -->
-
-                                    <!-- TrustBox widget - Micro Star -->
-                                    <div class="trustpilot-widget" 
-                                         data-locale="en-GB" 
-                                         data-template-id="5419b732fbfb950b10de65e5" 
-                                         data-businessunit-id="65957e521d1c310aa125e6d5" 
-                                         data-style-height="24px" 
-                                         data-style-width="100%" 
-                                         data-theme="dark">
-                                        <a href="https://uk.trustpilot.com/review/beebu.co.uk" target="_blank" rel="noopener">Trustpilot</a>
-                                    </div>
-                                    <!-- End TrustBox widget -->
-                                </div>
-                            <?php endif; ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        <?php endforeach;
-    endif; ?>
-  </div>
-
-  <div class="wrapper">
-    <div class="hero__inner">
-
-      <!-- <div class="hero__mobile-image hide-desktop">
-        <img src="<?= $mobile_image['url'] ?>" alt="">
-      </div>  -->
-
-      <h1 class="hero__title"><?= $title ?></h1>
-      <p class="hero__copy"><?= $copy ?></p>
-      
-      <?php if ($postcode_search): ?>
-        <form class="postcode-search" method="GET" action="<?php echo esc_url($availability_check_url); ?>"> 
-          <input class="postcode-search__input" type="text" name="postcode" value="<?php echo isset($_GET['postcode']) ? esc_attr($_GET['postcode']) : ''; ?>" placeholder="Enter your postcode" required> 
-            <button class="postcode-search__button" type="submit">Check Availability</button>
-         </form>
-       <?php endif; ?>
-
-       <?php if ( $additional_text_below_the_postcode_search ): ?>
-        <p class="hero__text-below-postcode"><?= $additional_text_below_the_postcode_search ?></p>
-      <?php endif; ?>
-
-      <?php if ( $add_page_button ) : ?>
-        <a href="<?=$button['url'] ?>" class="button <?php if ( $hero_type === 'flourish' ) : ?> button--black <?php endif; ?>"><?=$button['title'] ?></a>
-      <?php endif; ?>
-
-      <?php if ( $trustpilot ) : ?>
-        <div class="hero__trustpilot">
-          <!-- TrustBox script -->
-          <script type="text/javascript" src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" async></script>
-          <!-- End TrustBox script -->
-
-          <!-- TrustBox widget - Micro Star -->
-          <div class="trustpilot-widget" 
-               data-locale="en-GB" 
-               data-template-id="5419b732fbfb950b10de65e5" 
-               data-businessunit-id="65957e521d1c310aa125e6d5" 
-               data-style-height="24px" 
-               data-style-width="100%" 
-               data-theme="dark">
-            <a href="https://uk.trustpilot.com/review/beebu.co.uk" target="_blank" rel="noopener">Trustpilot</a>
-          </div>
-          <!-- End TrustBox widget -->
-        </div>
-      <?php endif; ?>
-        
-    </div>
-  </div>
-
-</div>
-
-<div class="mobile__hero">
-    <?php 
-    // Debug output
-    echo "<!-- Number of mobile images: " . count($mobile_images) . " -->";
-    ?>
-    
-    <div class="mobile-slider">
+    <div class="desktop-slider">
         <?php 
-        if ($mobile_images) :
-            $slide_count = 0;
-            foreach ($mobile_images as $image) : 
-                $slide_count++;
-                ?>
-                <div class="slide">
-                    <?php 
-                    // Debug output for each slide
-                    echo "<!-- Slide {$slide_count} URL: " . esc_url($image['mobile_image']['url']) . " -->";
-                    ?>
-                    <div class="hero-content mobile-hero-content" style="background-image: url('<?php echo esc_url($image['mobile_image']['url']); ?>');">
-                        <div class="wrapper">
-                            <div class="hero__inner">
-                                <h1 class="hero__title"><?= $title ?></h1>
-                                <p class="hero__copy"><?= $copy ?></p>
-                                <?php if ($postcode_search): ?>
-                                    <form class="postcode-search" method="GET" action="<?php echo esc_url($availability_check_url); ?>"> 
-                                        <input class="postcode-search__input" type="text" name="postcode" value="<?php echo isset($_GET['postcode']) ? esc_attr($_GET['postcode']) : ''; ?>" placeholder="Enter your postcode" required> 
-                                        <button class="postcode-search__button" type="submit">Check Availability</button>
-                                    </form>
-                                <?php endif; ?>
-                                <?php if ( $additional_text_below_the_postcode_search ): ?>
-                                    <p class="hero__text-below-postcode"><?= $additional_text_below_the_postcode_search ?></p>
-                                <?php endif; ?>
-                                <?php if ( $add_page_button ) : ?>
-                                    <a href="<?=$button['url'] ?>" class="button <?php if ( $hero_type === 'flourish' ) : ?> button--black <?php endif; ?>"><?=$button['title'] ?></a>
-                                <?php endif; ?>
-                                <?php if ( $trustpilot ) : ?>
-                                    <div class="hero__trustpilot">
-                                        <!-- TrustBox script -->
-                                        <script type="text/javascript" src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" async></script>
-                                        <!-- End TrustBox script -->
-
-                                        <!-- TrustBox widget - Micro Star -->
-                                        <div class="trustpilot-widget" 
-                                             data-locale="en-GB" 
-                                             data-template-id="5419b732fbfb950b10de65e5" 
-                                             data-businessunit-id="65957e521d1c310aa125e6d5" 
-                                             data-style-height="24px" 
-                                             data-style-width="100%" 
-                                             data-theme="dark">
-                                            <a href="https://uk.trustpilot.com/review/beebu.co.uk" target="_blank" rel="noopener">Trustpilot</a>
-                                        </div>
-                                        <!-- End TrustBox widget -->
-                                    </div>
-                                <?php endif; ?>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        if ($background_images) :
+            foreach ($background_images as $image) : ?>
+                <div class="slide" style="background-image: url('<?php echo esc_url($image['background_image']['url']); ?>');"></div>
             <?php endforeach;
         endif; ?>
     </div>
+
+    <div class="wrapper">
+        <div class="hero__inner">
+            <h1 class="hero__title"><?= $title ?></h1>
+            <p class="hero__copy"><?= $copy ?></p>
+            
+            <?php if ($postcode_search): ?>
+                <form class="postcode-search" method="GET" action="<?php echo esc_url($availability_check_url); ?>"> 
+                    <input class="postcode-search__input" type="text" name="postcode" value="<?php echo isset($_GET['postcode']) ? esc_attr($_GET['postcode']) : ''; ?>" placeholder="Enter your postcode" required> 
+                    <button class="postcode-search__button" type="submit">Check Availability</button>
+                </form>
+            <?php endif; ?>
+
+            <?php if ($additional_text_below_the_postcode_search): ?>
+                <p class="hero__text-below-postcode"><?= $additional_text_below_the_postcode_search ?></p>
+            <?php endif; ?>
+
+            <?php if ($add_page_button) : ?>
+                <a href="<?=$button['url'] ?>" class="button <?php if ($hero_type === 'flourish') : ?> button--black <?php endif; ?>"><?=$button['title'] ?></a>
+            <?php endif; ?>
+
+            <?php if ($trustpilot) : ?>
+                <div class="hero__trustpilot">
+                    <!-- TrustBox script -->
+                    <script type="text/javascript" src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" async></script>
+                    <div class="trustpilot-widget" data-locale="en-GB" data-template-id="5419b732fbfb950b10de65e5" data-businessunit-id="65957e521d1c310aa125e6d5" data-style-height="24px" data-style-width="100%" data-theme="dark">
+                        <a href="https://uk.trustpilot.com/review/beebu.co.uk" target="_blank" rel="noopener">Trustpilot</a>
+                    </div>
+                </div>
+            <?php endif; ?>
+        </div>
+    </div>
 </div>
 
-<style>
-.mobile__hero {
-    width: 100%;
-    height: 100vh;
-}
+<!--Mobile Hero-->
+<div class="mobile__hero">
+    <div class="mobile-slider">
+        <?php 
+        if ($mobile_images) :
+            foreach ($mobile_images as $image) : ?>
+                <div class="slide" style="background-image: url('<?php echo esc_url($image['mobile_image']['url']); ?>');"></div>
+            <?php endforeach;
+        endif; ?>
+    </div>
 
-.mobile-slider {
-    height: 100vh;
-}
+    <div class="wrapper">
+        <div class="hero__inner">
+            <h1 class="hero__title"><?= $title ?></h1>
+            <p class="hero__copy"><?= $copy ?></p>
+            
+            <?php if ($postcode_search): ?>
+                <form class="postcode-search" method="GET" action="<?php echo esc_url($availability_check_url); ?>"> 
+                    <input class="postcode-search__input" type="text" name="postcode" value="<?php echo isset($_GET['postcode']) ? esc_attr($_GET['postcode']) : ''; ?>" placeholder="Enter your postcode" required> 
+                    <button class="postcode-search__button" type="submit">Check Availability</button>
+                </form>
+            <?php endif; ?>
 
-.mobile-slider .slide {
-    height: 100vh !important;
-}
+            <?php if ($additional_text_below_the_postcode_search): ?>
+                <p class="hero__text-below-postcode"><?= $additional_text_below_the_postcode_search ?></p>
+            <?php endif; ?>
 
-.mobile-hero-content {
-    height: 100vh !important;
-    background-size: cover !important;
-    background-position: center !important;
-    background-repeat: no-repeat !important;
-}
+            <?php if ($add_page_button) : ?>
+                <a href="<?=$button['url'] ?>" class="button <?php if ($hero_type === 'flourish') : ?> button--black <?php endif; ?>"><?=$button['title'] ?></a>
+            <?php endif; ?>
 
-/* Slick specific styles */
-.mobile-slider .slick-list,
-.mobile-slider .slick-track {
-    height: 100% !important;
-}
-
-.mobile-slider .slick-dots {
-    bottom: 25px;
-    z-index: 1;
-}
-
-.mobile-slider .slick-dots li button:before {
-    color: white;
-}
-
-@media (max-width: 767px) {
-    .hero {
-        display: none !important;
-    }
-    .mobile__hero {
-        display: block !important;
-    }
-}
-
-/* Reset any existing slider styles */
-.mobile-slider {
-    opacity: 1 !important;
-    visibility: visible !important;
-}
-
-.mobile-slider .slide {
-    opacity: 1 !important;
-    position: relative !important;
-}
-
-/* Basic slider setup */
-.mobile-slider {
-    width: 100%;
-    height: 100vh;
-}
-
-.mobile-hero-content {
-    height: 100vh;
-    background-size: cover;
-    background-position: center;
-}
-
-/* Essential Slick styles */
-.slick-list, .slick-track {
-    height: 100%;
-}
-
-.slick-slide {
-    height: 100vh;
-}
-
-.slick-dots {
-    position: absolute;
-    bottom: 20px;
-    width: 100%;
-    text-align: center;
-    z-index: 1;
-}
-
-.slick-dots li {
-    display: inline-block;
-    margin: 0 5px;
-}
-
-@media (max-width: 767px) {
-    .hero {
-        display: none !important;
-    }
-    .mobile__hero {
-        display: block !important;
-    }
-}
-
-/* Desktop styles */
-.desktop-slider {
-    width: 100%;
-    height: 100vh;
-}
-
-.desktop-hero-content {
-    height: 100vh;
-    background-size: cover;
-    background-position: center;
-}
-
-/* Mobile styles */
-.mobile-slider {
-    width: 100%;
-    height: 100vh;
-}
-
-.mobile-hero-content {
-    height: 100vh;
-    background-size: cover;
-    background-position: center;
-}
-
-/* Common Slick styles */
-.slick-list, 
-.slick-track {
-    height: 100%;
-}
-
-.slick-slide {
-    height: 100vh;
-}
-
-.slick-dots {
-    position: absolute;
-    bottom: 20px;
-    width: 100%;
-    text-align: center;
-    z-index: 1;
-}
-
-.slick-dots li {
-    display: inline-block;
-    margin: 0 5px;
-}
-
-/* Media queries */
-@media (min-width: 768px) {
-    .mobile__hero {
-        display: none !important;
-    }
-    .hero {
-        display: block !important;
-    }
-}
-
-@media (max-width: 767px) {
-    .hero {
-        display: none !important;
-    }
-    .mobile__hero {
-        display: block !important;
-    }
-}
-</style>
+            <?php if ($trustpilot) : ?>
+                <div class="hero__trustpilot">
+                    <!-- TrustBox script -->
+                    <script type="text/javascript" src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" async></script>
+                    <div class="trustpilot-widget" data-locale="en-GB" data-template-id="5419b732fbfb950b10de65e5" data-businessunit-id="65957e521d1c310aa125e6d5" data-style-height="24px" data-style-width="100%" data-theme="dark">
+                        <a href="https://uk.trustpilot.com/review/beebu.co.uk" target="_blank" rel="noopener">Trustpilot</a>
+                    </div>
+                </div>
+            <?php endif; ?>
+        </div>
+    </div>
+</div>
 
 <script>
 jQuery(document).ready(function($) {
