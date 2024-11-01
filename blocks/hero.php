be0f9ebd67c96ea954a1c3776d6485754296f38b
Alex McIver <alexmciver@ymail.com>
2024-11-01 16:09:11 +0000

    Nearly there on the hero banners being different on mobile compared to desktop

--- blocks/hero.php
+++ blocks/hero.php
@@ -55,9 +55,72 @@ endif; ?>
   <div class="wrapper">
     <div class="hero__inner">
 
-      <div class="hero__mobile-image hide-desktop">
+      <!-- <div class="hero__mobile-image hide-desktop">
         <img src="<?= $mobile_image['url'] ?>" alt="">
-      </div> 
+      </div>  -->
+
+      <h1 class="hero__title"><?= $title ?></h1>
+      <p class="hero__copy"><?= $copy ?></p>
+      
+      <?php if ($postcode_search): ?>
+        <form class="postcode-search" method="GET" action="<?php echo esc_url($availability_check_url); ?>"> 
+          <input class="postcode-search__input" type="text" name="postcode" value="<?php echo isset($_GET['postcode']) ? esc_attr($_GET['postcode']) : ''; ?>" placeholder="Enter your postcode" required> 
+            <button class="postcode-search__button" type="submit">Check Availability</button>
+         </form>
+       <?php endif; ?>
+
+       <?php if ( $additional_text_below_the_postcode_search ): ?>
+        <p class="hero__text-below-postcode"><?= $additional_text_below_the_postcode_search ?></p>
+      <?php endif; ?>
+
+      <?php if ( $add_page_button ) : ?>
+        <a href="<?=$button['url'] ?>" class="button <?php if ( $hero_type === 'flourish' ) : ?> button--black <?php endif; ?>"><?=$button['title'] ?></a>
+      <?php endif; ?>
+
+      <?php if ( $trustpilot ) : ?>
+        <div class="hero__trustpilot">
+          <!-- TrustBox script -->
+          <script type="text/javascript" src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" async></script>
+          <!-- End TrustBox script -->
+
+          <!-- TrustBox widget - Micro Star -->
+          <div class="trustpilot-widget" 
+               data-locale="en-GB" 
+               data-template-id="5419b732fbfb950b10de65e5" 
+               data-businessunit-id="65957e521d1c310aa125e6d5" 
+               data-style-height="24px" 
+               data-style-width="100%" 
+               data-theme="dark">
+            <a href="https://uk.trustpilot.com/review/beebu.co.uk" target="_blank" rel="noopener">Trustpilot</a>
+          </div>
+          <!-- End TrustBox widget -->
+        </div>
+      <?php endif; ?>
+        
+    </div>
+  </div>
+
+</div>
+
+<div class="hero mobile__hero <?php if ( $hero_type === 'flourish' ) : echo 'flourish-hero'; elseif ( $hero_type === 'green-background' ) : echo 'section--green'; endif; ?>">
+
+  <div class="hero-slider" style="display: none;">
+    <?php 
+    $mobile_images = get_sub_field('mobile_images');
+    if ($mobile_images) :
+        foreach ($mobile_images as $image) : ?>
+            <div class="hero-slide" data-bg="<?= esc_url($image['mobile_image']['url']) ?>"></div>
+        <?php endforeach;
+    endif;
+    ?>
+  </div>
+
+  <div class="wrapper">
+    <div class="hero__inner">
+
+      <!-- <div class="hero__mobile-image hide-desktop">
+        <img src="<?= $mobile_image['url'] ?>" alt="">
+      </div>  -->
 
       <h1 class="hero__title"><?= $title ?></h1>
       <p class="hero__copy"><?= $copy ?></p>
