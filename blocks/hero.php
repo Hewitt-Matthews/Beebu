703a85efffb50195348d6c53c301a4a3ab50dfef
Alex McIver <alexmciver@ymail.com>
2024-11-01 17:38:11 +0000

    one step closer to what we want for the hero

--- blocks/hero.php
+++ blocks/hero.php
@@ -29,27 +29,68 @@ else :
   $availability_check_url = get_sub_field('availability_check_url');
   $additional_text_below_the_postcode_search = get_sub_field('additional_text_below_the_postcode_search');
   $background_images = get_sub_field('hero_background_images');
-  if ($background_images) :
-      foreach ($background_images as $image) :
-          $background_image = $image['background_image'];
-          break; // Assuming you only want the first image
-      endforeach;
-  endif;
+  $mobile_images = get_sub_field('mobile_images');
+
+  // Add debugging
+  echo '<pre style="display: none;">'; // Hidden but can check source
+  echo "Desktop Images:\n";
+  var_dump($background_images);
+  echo "\nMobile Images:\n";
+  var_dump($mobile_images);
+  echo '</pre>';
 
 endif; ?>
 
 
 <div class="hero <?php if ( $hero_type === 'flourish' ) : echo 'flourish-hero'; elseif ( $hero_type === 'green-background' ) : echo 'section--green'; endif; ?>">
 
-  <div class="hero-slider" style="display: none;">
+  <div class="desktop-slider">
     <?php 
-    $background_images = get_sub_field('hero_background_images');
     if ($background_images) :
         foreach ($background_images as $image) : ?>
-            <div class="hero-slide" data-bg="<?= esc_url($image['background_image']['url']) ?>"></div>
+            <div class="slide">
+                <div class="hero-content desktop-hero-content" style="background-image: url('<?php echo esc_url($image['background_image']['url']); ?>');">
+                    <div class="wrapper">
+                        <div class="hero__inner">
+                            <h1 class="hero__title"><?= $title ?></h1>
+                            <p class="hero__copy"><?= $copy ?></p>
+                            <?php if ($postcode_search): ?>
+                                <form class="postcode-search" method="GET" action="<?php echo esc_url($availability_check_url); ?>"> 
+                                    <input class="postcode-search__input" type="text" name="postcode" value="<?php echo isset($_GET['postcode']) ? esc_attr($_GET['postcode']) : ''; ?>" placeholder="Enter your postcode" required> 
+                                    <button class="postcode-search__button" type="submit">Check Availability</button>
+                                </form>
+                            <?php endif; ?>
+                            <?php if ( $additional_text_below_the_postcode_search ): ?>
+                                <p class="hero__text-below-postcode"><?= $additional_text_below_the_postcode_search ?></p>
+                            <?php endif; ?>
+                            <?php if ( $add_page_button ) : ?>
+                                <a href="<?=$button['url'] ?>" class="button <?php if ( $hero_type === 'flourish' ) : ?> button--black <?php endif; ?>"><?=$button['title'] ?></a>
+                            <?php endif; ?>
+                            <?php if ( $trustpilot ) : ?>
+                                <div class="hero__trustpilot">
+                                    <!-- TrustBox script -->
+                                    <script type="text/javascript" src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" async></script>
+                                    <!-- End TrustBox script -->
+
+                                    <!-- TrustBox widget - Micro Star -->
+                                    <div class="trustpilot-widget" 
+                                         data-locale="en-GB" 
+                                         data-template-id="5419b732fbfb950b10de65e5" 
+                                         data-businessunit-id="65957e521d1c310aa125e6d5" 
+                                         data-style-height="24px" 
+                                         data-style-width="100%" 
+                                         data-theme="dark">
+                                        <a href="https://uk.trustpilot.com/review/beebu.co.uk" target="_blank" rel="noopener">Trustpilot</a>
+                                    </div>
+                                    <!-- End TrustBox widget -->
+                                </div>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
         <?php endforeach;
-    endif;
-    ?>
+    endif; ?>
   </div>
 
   <div class="wrapper">
@@ -102,92 +143,290 @@ endif; ?>
 
 </div>
 
-<div class="hero mobile__hero <?php if ( $hero_type === 'flourish' ) : echo 'flourish-hero'; elseif ( $hero_type === 'green-background' ) : echo 'section--green'; endif; ?>">
-
-  <div class="hero-slider" style="display: none;">
+<div class="mobile__hero">
     <?php 
-    $mobile_images = get_sub_field('mobile_images');
-    if ($mobile_images) :
-        foreach ($mobile_images as $image) : ?>
-            <div class="hero-slide" data-bg="<?= esc_url($image['mobile_image']['url']) ?>"></div>
-        <?php endforeach;
-    endif;
+    // Debug output
+    echo "<!-- Number of mobile images: " . count($mobile_images) . " -->";
     ?>
-  </div>
+    
+    <div class="mobile-slider">
+        <?php 
+        if ($mobile_images) :
+            $slide_count = 0;
+            foreach ($mobile_images as $image) : 
+                $slide_count++;
+                ?>
+                <div class="slide">
+                    <?php 
+                    // Debug output for each slide
+                    echo "<!-- Slide {$slide_count} URL: " . esc_url($image['mobile_image']['url']) . " -->";
+                    ?>
+                    <div class="hero-content mobile-hero-content" style="background-image: url('<?php echo esc_url($image['mobile_image']['url']); ?>');">
+                        <div class="wrapper">
+                            <div class="hero__inner">
+                                <h1 class="hero__title"><?= $title ?></h1>
+                                <p class="hero__copy"><?= $copy ?></p>
+                                <?php if ($postcode_search): ?>
+                                    <form class="postcode-search" method="GET" action="<?php echo esc_url($availability_check_url); ?>"> 
+                                        <input class="postcode-search__input" type="text" name="postcode" value="<?php echo isset($_GET['postcode']) ? esc_attr($_GET['postcode']) : ''; ?>" placeholder="Enter your postcode" required> 
+                                        <button class="postcode-search__button" type="submit">Check Availability</button>
+                                    </form>
+                                <?php endif; ?>
+                                <?php if ( $additional_text_below_the_postcode_search ): ?>
+                                    <p class="hero__text-below-postcode"><?= $additional_text_below_the_postcode_search ?></p>
+                                <?php endif; ?>
+                                <?php if ( $add_page_button ) : ?>
+                                    <a href="<?=$button['url'] ?>" class="button <?php if ( $hero_type === 'flourish' ) : ?> button--black <?php endif; ?>"><?=$button['title'] ?></a>
+                                <?php endif; ?>
+                                <?php if ( $trustpilot ) : ?>
+                                    <div class="hero__trustpilot">
+                                        <!-- TrustBox script -->
+                                        <script type="text/javascript" src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" async></script>
+                                        <!-- End TrustBox script -->
 
-  <div class="wrapper">
-    <div class="hero__inner">
-
-      <!-- <div class="hero__mobile-image hide-desktop">
-        <img src="<?= $mobile_image['url'] ?>" alt="">
-      </div>  -->
-
-      <h1 class="hero__title"><?= $title ?></h1>
-      <p class="hero__copy"><?= $copy ?></p>
-      
-      <?php if ($postcode_search): ?>
-        <form class="postcode-search" method="GET" action="<?php echo esc_url($availability_check_url); ?>"> 
-          <input class="postcode-search__input" type="text" name="postcode" value="<?php echo isset($_GET['postcode']) ? esc_attr($_GET['postcode']) : ''; ?>" placeholder="Enter your postcode" required> 
-            <button class="postcode-search__button" type="submit">Check Availability</button>
-         </form>
-       <?php endif; ?>
-
-       <?php if ( $additional_text_below_the_postcode_search ): ?>
-        <p class="hero__text-below-postcode"><?= $additional_text_below_the_postcode_search ?></p>
-      <?php endif; ?>
-
-      <?php if ( $add_page_button ) : ?>
-        <a href="<?=$button['url'] ?>" class="button <?php if ( $hero_type === 'flourish' ) : ?> button--black <?php endif; ?>"><?=$button['title'] ?></a>
-      <?php endif; ?>
-
-      <?php if ( $trustpilot ) : ?>
-        <div class="hero__trustpilot">
-          <!-- TrustBox script -->
-          <script type="text/javascript" src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" async></script>
-          <!-- End TrustBox script -->
-
-          <!-- TrustBox widget - Micro Star -->
-          <div class="trustpilot-widget" 
-               data-locale="en-GB" 
-               data-template-id="5419b732fbfb950b10de65e5" 
-               data-businessunit-id="65957e521d1c310aa125e6d5" 
-               data-style-height="24px" 
-               data-style-width="100%" 
-               data-theme="dark">
-            <a href="https://uk.trustpilot.com/review/beebu.co.uk" target="_blank" rel="noopener">Trustpilot</a>
-          </div>
-          <!-- End TrustBox widget -->
-        </div>
-      <?php endif; ?>
-        
+                                        <!-- TrustBox widget - Micro Star -->
+                                        <div class="trustpilot-widget" 
+                                             data-locale="en-GB" 
+                                             data-template-id="5419b732fbfb950b10de65e5" 
+                                             data-businessunit-id="65957e521d1c310aa125e6d5" 
+                                             data-style-height="24px" 
+                                             data-style-width="100%" 
+                                             data-theme="dark">
+                                            <a href="https://uk.trustpilot.com/review/beebu.co.uk" target="_blank" rel="noopener">Trustpilot</a>
+                                        </div>
+                                        <!-- End TrustBox widget -->
+                                    </div>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach;
+        endif; ?>
     </div>
-  </div>
-
 </div>
 
-<script type="text/javascript">
-  jQuery(document).ready(function($){
-    var $hero = $('.hero');
-    var $slider = $('.hero-slider');
+<style>
+.mobile__hero {
+    width: 100%;
+    height: 100vh;
+}
 
-    $slider.slick({
-      dots: true,
-      infinite: true,
-      speed: 500,
-      cssEase: 'linear',
-      autoplay: true,
-      autoplaySpeed: 2000,
-      arrows: false // Optional: Hide navigation arrows if not needed
+.mobile-slider {
+    height: 100vh;
+}
+
+.mobile-slider .slide {
+    height: 100vh !important;
+}
+
+.mobile-hero-content {
+    height: 100vh !important;
+    background-size: cover !important;
+    background-position: center !important;
+    background-repeat: no-repeat !important;
+}
+
+/* Slick specific styles */
+.mobile-slider .slick-list,
+.mobile-slider .slick-track {
+    height: 100% !important;
+}
+
+.mobile-slider .slick-dots {
+    bottom: 25px;
+    z-index: 1;
+}
+
+.mobile-slider .slick-dots li button:before {
+    color: white;
+}
+
+@media (max-width: 767px) {
+    .hero {
+        display: none !important;
+    }
+    .mobile__hero {
+        display: block !important;
+    }
+}
+
+/* Reset any existing slider styles */
+.mobile-slider {
+    opacity: 1 !important;
+    visibility: visible !important;
+}
+
+.mobile-slider .slide {
+    opacity: 1 !important;
+    position: relative !important;
+}
+
+/* Basic slider setup */
+.mobile-slider {
+    width: 100%;
+    height: 100vh;
+}
+
+.mobile-hero-content {
+    height: 100vh;
+    background-size: cover;
+    background-position: center;
+}
+
+/* Essential Slick styles */
+.slick-list, .slick-track {
+    height: 100%;
+}
+
+.slick-slide {
+    height: 100vh;
+}
+
+.slick-dots {
+    position: absolute;
+    bottom: 20px;
+    width: 100%;
+    text-align: center;
+    z-index: 1;
+}
+
+.slick-dots li {
+    display: inline-block;
+    margin: 0 5px;
+}
+
+@media (max-width: 767px) {
+    .hero {
+        display: none !important;
+    }
+    .mobile__hero {
+        display: block !important;
+    }
+}
+
+/* Desktop styles */
+.desktop-slider {
+    width: 100%;
+    height: 100vh;
+}
+
+.desktop-hero-content {
+    height: 100vh;
+    background-size: cover;
+    background-position: center;
+}
+
+/* Mobile styles */
+.mobile-slider {
+    width: 100%;
+    height: 100vh;
+}
+
+.mobile-hero-content {
+    height: 100vh;
+    background-size: cover;
+    background-position: center;
+}
+
+/* Common Slick styles */
+.slick-list, 
+.slick-track {
+    height: 100%;
+}
+
+.slick-slide {
+    height: 100vh;
+}
+
+.slick-dots {
+    position: absolute;
+    bottom: 20px;
+    width: 100%;
+    text-align: center;
+    z-index: 1;
+}
+
+.slick-dots li {
+    display: inline-block;
+    margin: 0 5px;
+}
+
+/* Media queries */
+@media (min-width: 768px) {
+    .mobile__hero {
+        display: none !important;
+    }
+    .hero {
+        display: block !important;
+    }
+}
+
+@media (max-width: 767px) {
+    .hero {
+        display: none !important;
+    }
+    .mobile__hero {
+        display: block !important;
+    }
+}
+</style>
+
+<script>
+jQuery(document).ready(function($) {
+    // Initialize both sliders independently
+    function initSliders() {
+        // Desktop Slider
+        if (!$('.desktop-slider').hasClass('slick-initialized')) {
+            $('.desktop-slider').slick({
+                dots: true,
+                arrows: false,
+                infinite: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                fade: false,
+                speed: 300,
+                autoplay: true,
+                autoplaySpeed: 3000
+            });
+            console.log('Desktop slider initialized');
+        }
+
+        // Mobile Slider
+        if (!$('.mobile-slider').hasClass('slick-initialized')) {
+            $('.mobile-slider').slick({
+                dots: true,
+                arrows: false,
+                infinite: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                fade: false,
+                speed: 300,
+                autoplay: true,
+                autoplaySpeed: 3000
+            });
+            console.log('Mobile slider initialized');
+        }
+    }
+
+    // Initialize on page load
+    initSliders();
+
+    // Handle resize
+    var resizeTimer;
+    $(window).on('resize', function() {
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(function() {
+            // Destroy both sliders
+            if ($('.desktop-slider').hasClass('slick-initialized')) {
+                $('.desktop-slider').slick('unslick');
+            }
+            if ($('.mobile-slider').hasClass('slick-initialized')) {
+                $('.mobile-slider').slick('unslick');
+            }
+            // Reinitialize both
+            initSliders();
+        }, 250);
     });
-
-    // Set initial background
-    var initialBg = $slider.find('.hero-slide').eq(0).data('bg');
-    $hero.css('background-image', 'url(' + initialBg + ')');
-
-    // Update background on slide change
-    $slider.on('afterChange', function(event, slick, currentSlide){
-      var currentBg = $slider.find('.hero-slide').eq(currentSlide).data('bg');
-      $hero.css('background-image', 'url(' + currentBg + ')');
-    });
-  });
+});
 </script>
