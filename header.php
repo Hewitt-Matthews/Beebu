8265ae45e696daa5397cd6db3b08c4bf3bf3e9aa
Alex McIver <alexmciver@ymail.com>
2025-01-29 12:52:06 +0000

    Logo changes different on mobile and desktop if selected

--- header.php
+++ header.php
@@ -26,24 +26,27 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <div class="header__inner">
             <div class="header__logo">
             <?php 
-              $nav_img = get_field('header_nav_image');
-              if ( $nav_img ) { ?>
-                  <a href="/">
-                      <img src="<?= $nav_img ?>" />
-                  </a>
-              <?php } else { ?>
-                  <a href="/">
-                      <?php //home, about, contact
-                      if ( is_page('about') || is_page('contact') || is_home() || is_front_page() ) : ?>
-                          <img src="<?=get_template_directory_uri() ?>/assets/img/new-beebu-logo-white.png" />
-                      <?php elseif ( is_404() ) : ?>
-                          <img src="<?=get_template_directory_uri() ?>/assets/img/beebu-logo-white.svg" />
-                      <?php else : ?>
-                          <img src="<?=get_template_directory_uri() ?>/assets/img/new-beebu-logo-black.png" />
-                      <?php endif; ?>
-                  </a>
-              <?php }
-              ?>
+              $desktop_logo = get_field('header_nav_image');
+              $mobile_logo = get_field('header_nav_image_mobile');
+            ?>
+            <a href="/">
+                <?php if ($desktop_logo && isset($desktop_logo['url'])): ?>
+                    <img src="<?= esc_url($desktop_logo['url']); ?>" alt="<?= esc_attr($desktop_logo['alt'] ?? ''); ?>" class="desktop-logo" />
+                    <img src="<?= esc_url($mobile_logo && isset($mobile_logo['url']) ? $mobile_logo['url'] : $desktop_logo['url']); ?>" alt="<?= esc_attr($mobile_logo && isset($mobile_logo['alt']) ? $mobile_logo['alt'] : ($desktop_logo['alt'] ?? '')); ?>" class="mobile-logo" />
+                <?php else: ?>
+                    <?php // Use specific logos based on page type
+                    if ( is_page('about') || is_page('contact') || is_home() || is_front_page() ) : ?>
+                        <img src="<?=get_template_directory_uri() ?>/assets/img/new-beebu-logo-white.png" class="desktop-logo" />
+                        <img src="<?=get_template_directory_uri() ?>/assets/img/new-beebu-logo-white.png" class="mobile-logo" />
+                    <?php elseif ( is_404() ) : ?>
+                        <img src="<?=get_template_directory_uri() ?>/assets/img/beebu-logo-white.svg" class="desktop-logo" />
+                        <img src="<?=get_template_directory_uri() ?>/assets/img/beebu-logo-white.svg" class="mobile-logo" />
+                    <?php else : ?>
+                        <img src="<?=get_template_directory_uri() ?>/assets/img/new-beebu-logo-black.png" class="desktop-logo" />
+                        <img src="<?=get_template_directory_uri() ?>/assets/img/new-beebu-logo-black.png" class="mobile-logo" />
+                    <?php endif; ?>
+                <?php endif; ?>
+            </a>
 
             </div>
             <nav aria-label="Main Menu">
